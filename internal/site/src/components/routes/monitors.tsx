298ab7905872ea446a2339903a2e71a3ef04e3f5
brandenk514 <12478583+brandenk514@users.noreply.github.com.>
2026-08-23 13:24:57 -0600

    fix(site): make Add Monitor button open dialog (remove stale useMemo cache)

--- internal/site/src/components/routes/monitors.tsx
+++ internal/site/src/components/routes/monitors.tsx
@@ -1,5 +1,5 @@
 import { Trans, useLingui } from "@lingui/react/macro"
-import { memo, Suspense, useEffect, useMemo, useState } from "react"
+import { memo, Suspense, useEffect, useState } from "react"
 import { PlusIcon } from "lucide-react"
 import MonitorsTable from "@/components/monitors-table/monitors-table"
 import { AddMonitorDialog } from "@/components/add-monitor"
@@ -15,26 +15,23 @@ export default memo(() => {
 		document.title = `${t`Monitors`} / Beszel`
 	}, [t])
 
-	return useMemo(
-		() => (
-			<>
-				<AddMonitorDialog open={dialogOpen} setOpen={setDialogOpen} />
-				<div className="flex flex-col gap-4">
-					{!isReadOnlyUser() && (
-						<div className="flex justify-end">
-							<Button onClick={() => setDialogOpen(true)} className="flex gap-1">
-								<PlusIcon className="h-4 w-4 -ms-1" />
-								<Trans>Add Monitor</Trans>
-							</Button>
-						</div>
-					)}
-					<Suspense>
-						<MonitorsTable />
-					</Suspense>
-				</div>
-				<FooterRepoLink />
-			</>
-		),
-		[]
+	return (
+		<>
+			<AddMonitorDialog open={dialogOpen} setOpen={setDialogOpen} />
+			<div className="flex flex-col gap-4">
+				{!isReadOnlyUser() && (
+					<div className="flex justify-end">
+						<Button onClick={() => setDialogOpen(true)} className="flex gap-1">
+							<PlusIcon className="h-4 w-4 -ms-1" />
+							<Trans>Add Monitor</Trans>
+						</Button>
+					</div>
+				)}
+				<Suspense>
+					<MonitorsTable />
+				</Suspense>
+			</div>
+			<FooterRepoLink />
+		</>
 	)
 })
