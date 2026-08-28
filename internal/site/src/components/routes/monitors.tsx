d002fac1f8c414c5c18631762a48f6a257bdbc03
brandenk514 <12478583+brandenk514@users.noreply.github.com.>
2026-08-28 09:20:43 -0600

    Removed white add monitor button

--- internal/site/src/components/routes/monitors.tsx
+++ internal/site/src/components/routes/monitors.tsx
@@ -1,15 +1,10 @@
-import { Trans, useLingui } from "@lingui/react/macro"
-import { memo, Suspense, useEffect, useState } from "react"
-import { PlusIcon } from "lucide-react"
+import { useLingui } from "@lingui/react/macro"
+import { memo, Suspense, useEffect } from "react"
 import MonitorsTable from "@/components/monitors-table/monitors-table"
-import { AddMonitorDialog } from "@/components/add-monitor"
-import { Button } from "@/components/ui/button"
-import { isReadOnlyUser } from "@/lib/api"
 import { FooterRepoLink } from "@/components/footer-repo-link"
 
 export default memo(() => {
 	const { t } = useLingui()
-	const [dialogOpen, setDialogOpen] = useState(false)
 
 	useEffect(() => {
 		document.title = `${t`Monitors`} / Beszel`
@@ -17,16 +12,7 @@ export default memo(() => {
 
 	return (
 		<>
-			<AddMonitorDialog open={dialogOpen} setOpen={setDialogOpen} />
 			<div className="flex flex-col gap-4">
-				{!isReadOnlyUser() && (
-					<div className="flex justify-end">
-						<Button onClick={() => setDialogOpen(true)} className="flex gap-1">
-							<PlusIcon className="h-4 w-4 -ms-1" />
-							<Trans>Add Monitor</Trans>
-						</Button>
-					</div>
-				)}
 				<Suspense>
 					<MonitorsTable />
 				</Suspense>
